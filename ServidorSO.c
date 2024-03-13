84c336ef5566b93b8d45f09db0e8b80c513edff1
antonia <felix.antonio.perez@estudiantat.upc.edu>
2024-03-14 00:59:14 +0100

    primera version

---
+++ ServidorSO.c
@@ -0,0 +1,177 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <mysql.h>
+int main(int argc, char *argv[])
+{
+	int sock_conn, sock_listen, ret;
+	struct sockaddr_in serv_adr;
+	char buff[512];
+	char buff2[512];
+	// INICIALITZACIONS
+	// Obrim el socket
+	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		printf("Error creant socket");
+	// Fem el bind al port
+	
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
+	serv_adr.sin_family = AF_INET;
+	
+	// asocia el socket a cualquiera de las IP de la m?quina. 
+	//htonl formatea el numero que recibe al formato necesario
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// escucharemos en el port 9050
+	serv_adr.sin_port = htons(9080);
+	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+		printf ("Error al bind");
+	//La cola de peticiones pendientes no podr? ser superior a 4
+	if (listen(sock_listen, 2) < 0)
+		printf("Error en el Listen");
+	
+	//Inicio el MYSQL
+	MYSQL *conn;
+	int err;
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexi\ufff3n: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexin
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "parchis",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	
+	
+	int i;
+	// Atenderemos solo 5 peticione
+	for(i=0;i<7;i++){
+		printf ("Escuchando\n");
+		
+		sock_conn = accept(sock_listen, NULL, NULL);
+		printf ("He recibido conexi?n\n");
+		//sock_conn es el socket que usaremos para este cliente
+		
+		// Ahora recibimos su nombre, que dejamos en buff
+		ret=read(sock_conn,buff, sizeof(buff));
+		printf ("Recibido\n");
+		
+		// Tenemos que a?adirle la marca de fin de string 
+		// para que no escriba lo que hay despues en el buffer
+		buff[ret]='\0';
+		
+		//Escribimos el nombre en la consola
+		
+		printf ("Se ha conectado: %s\n",buff);
+		
+		
+		
+		
+		
+		char *p = strtok( buff, "/");
+		int codigo =  atoi (p);
+		
+		
+		if (codigo == 1) //piden la longitd del nombre
+		{
+			p = strtok( NULL, "/");
+			char nombre[20];
+			strcpy (nombre, p);
+			printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
+			sprintf (buff2,"%d,",strlen(nombre));
+		}
+		else if(codigo ==2)//Codigo de inicio de secion.
+		{
+			//Codigo de inicio de secion.
+			char nomUsuario[20];
+			char contra[20];
+			
+			p= strtok(NULL, "/");
+			strcpy(nomUsuario, p);
+			
+			p= strtok(NULL, "/");
+			strcpy(contra, p);
+			
+			//faig la consulta
+			char consulta [1000];
+			strcpy(consulta, "SELECT usuario.Nombre_Usuario FROM usuario WHERE usuario.Nombre_Usuario = '");
+			strcat(consulta, nomUsuario);
+			strcat(consulta, "' AND usuario.Contraseña = '");
+			strcat(consulta, contra);
+			strcat(consulta, ";");
+			
+			err = mysql_query (conn, consulta);
+			if(err!=0){
+				printf ("Error al consultar datos de la base %u %s\n", mysql_errno(conn), mysql_error(conn));
+						
+				exit (1);
+			}
+			//Recollim el resultat de la consulta.
+			resultado = mysql_store_result(conn);
+			//ahora busco la fila de del usuario.
+			row = mysql_fetch_row (resultado);
+			
+			//si la contrasenya i el nom d'usuari son correctes no sera null.
+			if(row == NULL){
+				strcpy(buff2, "-1");
+				printf("No hi es!!!");
+			}
+			else{
+				strcpy(buff2, "0");
+				printf("Trobat!!");
+			}
+			
+			// Y lo enviamos
+			write (sock_conn,buff2, strlen(buff2));
+		}
+		else if(codigo==3)
+		{
+			//Codigo de inicio de secion.
+			char nomUsuario[20];
+			int puntos;
+			
+			p= strtok(NULL, "/");
+			strcpy(nomUsuario, p);
+			
+			
+			//faig la consulta
+			char consulta [1000];
+			strcpy(consulta, "SELECT puntos FROM usuario WHERE usuario.Nombre_Usuario = '");
+			strcat(consulta, nomUsuario);
+			strcat(consulta, ";");
+			
+			err = mysql_query (conn, consulta);
+			if(err!=0){
+				printf ("Error al consultar datos de la base %u %s\n", mysql_errno(conn), mysql_error(conn));
+				
+				exit (1);
+			}
+			//Recollim el resultat de la consulta.
+			resultado = mysql_store_result(conn);
+			//ahora busco la fila de del usuario.
+			row = mysql_fetch_row (resultado);
+			
+			strcpy(buff2, row);
+			
+			// Y lo enviamos
+			write (sock_conn,buff2, strlen(buff2));
+		}
+		printf ("%s\n", buff2);
+		
+		
+		// Se acabo el servicio para este cliente
+		close(sock_conn); 
+	}
+}
---
+++ cliente/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        public string correo, nombreUsuario, contraseña;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse(IP.Text);
+            IPEndPoint ipep = new IPEndPoint(direc, 9080);
+            
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (consulta1.Checked)
+            {
+                string mensaje = "1/" + nombreUsuario + "/" + contraseña + "/" + correo;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+                MessageBox.Show("La respuesta a la consulta 1 és: " + mensaje);
+            }
+            else if (consulta2.Checked)
+            {
+                string mensaje = "1/" + nombreUsuario + "/" + contraseña + "/" + correo;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split (',')[0];
+                MessageBox.Show("La respuesta a la consulta 2 és: " + mensaje);
+            }
+            else if (consulta3.Checked)
+            {
+                string mensaje = "1/" + nombreUsuario + "/" + contraseña + "/" + correo;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                MessageBox.Show("La respuesta a la consulta 3 és: " + mensaje);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una consulta.");
+            }
+
+            // Se terminó el servicio. 
+            // Nos desconectamos
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+
+        }
+
+        private void signupButton_Click(object sender, EventArgs e)
+        {
+            consultasBox.Visible = false;
+            signupLabel.Visible = true;
+            signupCorreoLabel.Visible = true;
+            signupUsuarioLabel.Visible = true;
+            signupContraseñaLabel.Visible = true;
+            signupCorreotextBox.Visible = true;
+            signupUsuariotextBox.Visible = true;
+            signupContraseñatextBox.Visible = true;
+            signupRegistrarButton.Visible = true;
+
+            loginLabel.Visible = false;
+            loginUsuarioLabel.Visible = false;
+            loginContraseñaLabel.Visible = false;
+            loginUsuariotextBox.Visible = false;
+            loginContraseñatextBox.Visible = false;
+            loginEntrarButton.Visible = false;
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            consultasBox.Visible = false;
+            loginLabel.Visible = true;
+            loginUsuarioLabel.Visible = true;
+            loginContraseñaLabel.Visible = true;
+            loginUsuariotextBox.Visible = true;
+            loginContraseñatextBox.Visible = true;
+            loginEntrarButton.Visible = true;
+
+            signupLabel.Visible = false;
+            signupCorreoLabel.Visible = false;
+            signupUsuarioLabel.Visible = false;
+            signupContraseñaLabel.Visible = false;
+            signupCorreotextBox.Visible = false;
+            signupUsuariotextBox.Visible = false;
+            signupContraseñatextBox.Visible = false;
+            signupRegistrarButton.Visible = false;
+        }
+
+        private void loginEntrarButton_Click(object sender, EventArgs e)
+        {
+            if (loginUsuariotextBox.Text != "" && loginContraseñatextBox.Text != "")
+            {
+                //envio en la base de datos los datos intorduciodos;
+                string mensaje = $"2/{loginUsuariotextBox.Text}/{loginContraseñatextBox.Text}";
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //recivimos la respuesta del Server.
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+
+                if(mensaje == "0")
+                {
+                    MessageBox.Show("Bienvenido de nuevo, " + loginUsuariotextBox.Text + "!");
+                    consultasBox.Visible = true;
+                    consultasNombretextBox.Text = loginUsuariotextBox.Text;
+                    nombreUsuario = consultasNombretextBox.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Te has equivocado de contraseña o usuario.");
+                }
+
+                
+            }
+            else
+            {
+                MessageBox.Show("Debes rellenar todos los campos.");
+            }
+        }
+
+        private void signupRegistrarButton_Click(object sender, EventArgs e)
+        {
+            if (signupCorreotextBox.Text != "" && signupUsuariotextBox.Text != "" && signupContraseñatextBox.Text != "")
+            {
+                 
+
+                MessageBox.Show("Usuario registrado con éxito.");
+                consultasBox.Visible = true;
+                consultasNombretextBox.Text = signupUsuariotextBox.Text;
+                nombreUsuario = consultasNombretextBox.Text;
+            }
+            else
+            {
+                MessageBox.Show("Debes rellenar todos los campos.");
+            }
+        }
+    }
+}
